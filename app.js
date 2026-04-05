044b7405bbdc5268b6242d9244cf5a0f2e63a889
Palak Bansal <palakbansal5002@gmail.com>
2026-04-05 17:16:29 +0530

    added the feature to send facts on sunday

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const { Resend } = require('resend');
 const cron = require('node-cron');
 const path = require('path');
+const fetchNewsletterContent = require('./fetchNewsletterContent');
 dotenv.config();
 
 const app = express();
@@ -29,14 +30,23 @@ app.post('/subscribe', async (req, res) => {
         return res.status(400).send('Name and email are required');
     }
     try {
-        // const existing = await Subscriber.findOne({ email });
-        // if (!existing) 
+        const existing = await Subscriber.findOne({ email });
+        if (!existing) {
         await Subscriber.create({ name, email });
+        }
         await resend.emails.send({
-            from:"onboarding@resend.dev",
+            from: "onboarding@resend.dev",
             to: email,
             subject: 'Welcome to our Newsletter!',
-            text: `Hi ${name},\n\nThank you for subscribing! You will receive our emails every Sunday.`,
+            html: `<p>Hiii <b>${name}</b> :),</p>
+<p><b>Welcome to Chintu's!</b></p>
+<p><b>Thank you for subscribing to our Newsletter Service!</b></p>
+<p>From today onwards, you will receive our emails <b>every Sunday</b>.<br>
+The emails will be curated especially for you on the <b>basis</b> of what you search on Chintu and your <b>preferred field of knowledge</b>.</p>
+<p>Hope you like the emails and keep supporting <b><span style="color:#023430">CHINTU</span></b>.</p>
+<p>For any queries you can reach out to us on this email. We'll be more than happy to assist you.</p>
+<br>
+<p>Yours Truly,<br>Team Chintu</p>`
         });
         res.sendFile(path.join(__dirname, 'subscribed.html'));
     } catch (err) {
@@ -45,18 +55,30 @@ app.post('/subscribe', async (req, res) => {
     }
 });
 
-
-cron.schedule('0 10 * * 0', async () => {
-    const subscribers = await Subscriber.find();
-    for (const sub of subscribers) {
-        await resend.emails.send({
-            from: "onboarding@resend.dev",
-            to: sub.email,
-            subject: 'Your Weekly Newsletter',
-            text: `Hello ${sub.name},\n\nHere is your weekly newsletter!`,
-        });
+cron.schedule('0 12 * * 0', async () => {
+    try {
+        const subscribers = await Subscriber.find();
+        const facts = await fetchNewsletterContent();
+        let factsHtml = '';
+        if (facts.length > 0) {
+            factsHtml = '<h3>Interesting Facts of the Week:</h3><ul>' + facts.map(f => `<li>${f}</li>`).join('') + '</ul>';
+        } else {
+            factsHtml = '<p>No facts available this week.</p>';
+        }
+        for (const sub of subscribers) {
+            await resend.emails.send({
+                from: "onboarding@resend.dev",
+                to: sub.email,
+                subject: 'Your Weekly Newsletter',
+                html: `<p>Hiii <b>${sub.name}</b> :),</p>\n${factsHtml}\n<p>Thank you for subscribing to our Newsletter Service!</p>\n<p>Hope you like the emails and keep supporting <b><span style=\"color:#023430\">CHINTU</span></b>.</p>\n<p>For any queries you can reach out to us on this email. We'll be more than happy to assist you.</p>\n<br>\n<p>Yours Truly,<br>Team Chintu</p>`
+            });
+        }
+        console.log('Weekly emails sent');
+    } catch (err) {
+        console.error('Error in scheduled newsletter job:', err);
     }
-    console.log('Weekly emails sent');
+}, {
+    timezone: 'Asia/Kolkata'
 });
 
 const PORT = 3000;
